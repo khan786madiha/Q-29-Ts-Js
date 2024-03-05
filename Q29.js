8a9ff87fe185750ceb142d15c0f9958e781f634b
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-03-05 10:54:43 -0800

    Q-29 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-29

---
+++ Q29.js
@@ -0,0 +1,29 @@
+"use strict";
+// 29.	Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+Object.defineProperty(exports, "__esModule", { value: true });
+// Make an array of your three favorite fruits
+let favorite_fruits = ["apple", "banana", "orange"];
+// Check if each fruit is in your array and print a statement accordingly
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like oranges!");
+}
+if (favorite_fruits.includes("strawberry")) {
+    console.log("You really like strawberries!");
+}
+else {
+    console.log("You don't like strawberries as much.");
+}
+if (favorite_fruits.includes("kiwi")) {
+    console.log("You really like kiwis!");
+}
+else {
+    console.log("Kiwis are not one of your favorite fruits.");
+}
